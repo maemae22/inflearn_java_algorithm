dc1cdb77be82c183d06a6628a17039c70ebb08d2
maemae22 <maemae2@naver.com>
2023-02-21 20:19:51 +0900

    P02_대소문자변환 / 문제 설명 추가

--- 1. String(문자열)/P02_대소문자변환.java
+++ 1. String(문자열)/P02_대소문자변환.java
@@ -1,7 +1,8 @@
 import java.util.*;
 import java.io.*;
 
-// 대문자는 소문자로, 소문자는 대문자로
+// 대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+// ex. StuDY -> sTUdy
 public class P02_대소문자변환 {
 
     public static void main(String[] args) throws Exception {
