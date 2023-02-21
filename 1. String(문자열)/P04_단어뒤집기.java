35e12671958d5b6f0422c87440f927ad144d278b
maemae22 <maemae2@naver.com>
2023-02-21 20:20:16 +0900

    P04_단어뒤집기 / 문제 설명 추가

--- 1. String(문자열)/P04_단어뒤집기.java
+++ 1. String(문자열)/P04_단어뒤집기.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import java.io.*;
 
+// N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성하세요.
+// ex. Time -> emiT
 public class P04_단어뒤집기 {
 
     public static void main(String[] args) throws Exception {
