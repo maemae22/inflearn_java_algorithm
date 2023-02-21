a523aaf59f9bc4e4857fcb46c56629239a1012cc
maemae22 <maemae2@naver.com>
2023-02-21 20:20:07 +0900

    P03_문장속단어 / 문제 설명 추가

--- 1. String(문자열)/P03_문장속단어.java
+++ 1. String(문자열)/P03_문장속단어.java
@@ -1,7 +1,9 @@
 import java.util.*;
 import java.io.*;
 
-// 문장이 주어지면, 문장 속에서 제일 긴 단어를 찾아서 return.
+// 한 개의 문장이 주어지면 그 문장 속에서 가장 긴 단어를 출력하는 프로그램을 작성하세요.
+// 문장속의 각 단어는 공백으로 구분됩니다.
+// ex. it is time to study -> study
 public class P03_문장속단어 {
 
     public static void main(String[] args) throws Exception {
