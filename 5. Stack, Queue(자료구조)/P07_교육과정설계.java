b944f96b556c1b8d8e0a908cf22fdaf7cfc7363e
maemae22 <maemae2@naver.com>
2023-03-06 21:54:32 +0900

    P07_교육과정설계 (풀이 추가)

--- 5. Stack, Queue(자료구조)/P07_교육과정설계.java
+++ 5. Stack, Queue(자료구조)/P07_교육과정설계.java
@@ -18,6 +18,7 @@ public class P07_교육과정설계 {
             queue.offer(c);
         }
 
+        // 방법1.
         for (char c : letter.toCharArray()) {
             if (queue.isEmpty()) {
                 break;
@@ -32,5 +33,26 @@ public class P07_교육과정설계 {
         } else {
             System.out.println("NO");
         }
+
+        /*
+        // 방법2.
+        String answer = "YES";
+        for (char c : letter.toCharArray()) {
+            if (queue.contains(c)) { // 필수과목이면
+                if (queue.poll() != c) { // Queue에서 꺼내고, 필수과목 순서가 맞는지 확인한다.
+                    answer = "NO"; // 순서가 틀렸을 경우, NO로 답변을 바꾸고 break.
+                    break;
+                }
+            }
+        }
+
+        // 계획대로 수업을 모두 수강했는데, 필수과목에 과목이 남아있을 경우 => 수강하지 않은 필수과목이 있다는 뜻
+        if (!queue.isEmpty()) {
+            answer = "NO";
+        }
+
+        System.out.println(answer);
+         */
+
     }
 }
