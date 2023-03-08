7fa0491694d01047d205f75c52e40ae4d297fb27
maemae22 <maemae2@naver.com>
2023-03-08 15:18:39 +0900

    P05_중복확인 (풀이 추가)

--- 6. Sorting and Searching(정렬, 이분검색과 결정알고리즘)/P05_중복확인.java
+++ 6. Sorting and Searching(정렬, 이분검색과 결정알고리즘)/P05_중복확인.java
@@ -13,6 +13,7 @@ ex.
  */
 public class P05_중복확인 {
 
+    // 방법1. TreeSet 사용 (1)
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -28,6 +29,55 @@ public class P05_중복확인 {
         } else {
             System.out.println("D");
         }
+    }
+
+    // 방법2. TreeSet 사용 (2)
+    public static void main2(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        TreeSet<Integer> set = new TreeSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        boolean duplicate = false;
+        for (int i=0; i<N; i++) {
+            int push = Integer.parseInt(st.nextToken());
+            if (set.contains(push)) {
+                System.out.println("D");
+                duplicate = true;
+                break;
+            } else {
+                set.add(push);
+            }
+        }
+
+        if (!duplicate) {
+            System.out.println("U");
+        }
+    }
+
+    // 방법3. 정렬 이용 - O(nlogn)
+    public static void main3(String[] args) throws Exception {
+        // 초기 세팅
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 로직 (정렬 + 정렬 후 같은 것이 있는지 for문을 돌며 확인) : Arrays.sort(arr);
+        String answer = "U";
+        Arrays.sort(arr);
+
+        for (int i=1; i<arr.length; i++) {
+            if (arr[i] == arr[i-1]) {
+                answer = "D";
+                break;
+            }
+        }
+
+        System.out.println(answer);
     }
 
 }
