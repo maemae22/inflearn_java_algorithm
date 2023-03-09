60e9def20a9a64b6f467d9509a1ce39f9e73db11
maemae22 <maemae2@naver.com>
2023-03-09 19:05:13 +0900

    P04_피보나치수열_재귀_메모이제이션 (풀이 추가 : 메모이제이션 활용 ( + static 변수 활용 ))
    함수 1번만 호출하는 것은 동일하나, 이미 구했던 값은 저장해놓고 다시 구하지 않는다는 점에서 시간이 확 단축됨

--- 7. Recursive, Tree, Graph(DFS, BFS 기초)/P04_피보나치수열_재귀_메모이제이션.java
+++ 7. Recursive, Tree, Graph(DFS, BFS 기초)/P04_피보나치수열_재귀_메모이제이션.java
@@ -72,4 +72,37 @@ public class P04_피보나치수열_재귀_메모이제이션 {
         }
     }
 
+
+    // 방법3. ⭐⭐ 메모이제이션 활용 ⭐⭐ ( + static 변수 활용 )
+    // 메모이제이션(memoization) : 컴퓨터 프로그램이 동일한 계산을 반복해야 할 때, 이전에 계산한 값을 메모리에 저장함으로써
+    //                          동일한 계산의 반복 수행을 제거하여 프로그램 실행 속도를 빠르게 하는 기술이다.
+    //                          동적 계획법의 핵심이 되는 기술이다.
+    static int[] arr3;
+
+    public static void main3(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        arr3 = new int[N+1];
+
+        // 함수 1번만 호출하는 것은 동일하나, 이미 구했던 값은 저장해놓고 다시 구하지 않는다는 점에서 시간이 확 단축됨 !! 제일 좋다 ~!!
+        Pibo3(N);
+
+        for (int i=1; i<=N; i++) {
+            System.out.print(arr3[i] + " ");
+        }
+    }
+
+    public static int Pibo3(int N) {
+        if (arr3[N] != 0) {
+            return arr3[N];
+        }
+
+        if (N==1 || N==2) {
+            return arr3[N] = 1;
+        } else {
+            return arr3[N] = Pibo3(N-2) + Pibo3(N-1);
+        }
+    }
+
 }
