aa3173c71acd4076d133f8d37196b47592f09f07
maemae22 <maemae2@naver.com>
2023-03-30 01:54:35 +0900

    P05_특정문자뒤집기

---
+++ 1. String(문자열)/복습_다시풀기/P05_특정문자뒤집기.java
@@ -0,0 +1,33 @@
+package 복습_다시풀기;
+
+import java.util.*;
+import java.io.*;
+
+public class P05_특정문자뒤집기 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String letter = br.readLine();
+
+        int lt = 0;
+        int rt = letter.length()-1;
+        char[] arr = letter.toCharArray();
+
+        while (lt < rt) {
+            while (!Character.isAlphabetic(arr[lt])) {
+                lt++;
+            }
+            while (!Character.isAlphabetic(arr[rt])) {
+                rt--;
+            }
+            char c = arr[lt];
+            arr[lt] = arr[rt];
+            arr[rt] = c;
+            lt++;
+            rt--;
+        }
+
+        String answer = String.valueOf(arr);
+        System.out.println(answer);
+    }
+}
