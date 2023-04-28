ba4bcbd86315f50ff7d0d3f3726b4d4fe59dc013
maemae22 <maemae2@naver.com>
2023-04-28 14:16:30 +0900

    P02_보이는학생

---
+++ 2. Array(1, 2차원 배열)/복습_다시다시풀기/P02_보이는학생.java
@@ -0,0 +1,29 @@
+package 복습_다시다시풀기;
+
+import java.util.*;
+import java.io.*;
+
+public class P02_보이는학생 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = 0;
+        int count = 0;
+
+        for (int num : arr) {
+            if (max < num) {
+                max = num;
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
