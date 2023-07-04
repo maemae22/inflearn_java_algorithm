4e2e9db560bf94e076edc7dba7317a25c5e1ed6d
maemae22 <maemae2@naver.com>
2023-07-04 18:15:28 +0900

    P01_사다리타기 (풀이 추가)

--- 섹션 1. 시뮬레이션 & 구현/P01_사다리타기.java
+++ 섹션 1. 시뮬레이션 & 구현/P01_사다리타기.java
@@ -34,10 +34,35 @@ public class P01_사다리타기 {
         return answer;
     }
 
+    public static char[] solution2(int n, int[][] ladder){
+        char[] letters = new char[n];
+        for (int i=0; i<n; i++) {
+            letters[i] = (char)(65+i);
+        }
+
+        for (int i=0; i<ladder.length; i++) {
+            for (int j=0; j<ladder[i].length; j++) {
+                int num = ladder[i][j];
+
+                // letters 배열에서 num-1과 num의 index를 교환
+                char tmp = letters[num-1];
+                letters[num-1] = letters[num];
+                letters[num] = tmp;
+            }
+        }
+
+        return letters;
+    }
+
     public static void main(String[] args){
         System.out.println(Arrays.toString(solution(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
         System.out.println(Arrays.toString(solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
         System.out.println(Arrays.toString(solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
         System.out.println(Arrays.toString(solution(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
+        System.out.println();
+        System.out.println(Arrays.toString(solution2(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
+        System.out.println(Arrays.toString(solution2(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
+        System.out.println(Arrays.toString(solution2(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
+        System.out.println(Arrays.toString(solution2(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
     }
 }
