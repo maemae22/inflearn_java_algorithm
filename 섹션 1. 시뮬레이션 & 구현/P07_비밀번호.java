38e0a5f742e66138e4cdda6caf5c3135d7c6408a
maemae22 <maemae2@naver.com>
2023-07-17 21:48:56 +0900

    P07_비밀번호 (풀이 수정)

--- 섹션 1. 시뮬레이션 & 구현/P07_비밀번호.java
+++ 섹션 1. 시뮬레이션 & 구현/P07_비밀번호.java
@@ -1,37 +1,34 @@
 import java.util.*;
 
 public class P07_비밀번호 {
-    public int solution(int[] keypad, String password){
+
+    public int solution(int[] keypad, String password) {
         int answer = 0;
-        int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-        int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
-        int[][] pad = new int[3][3];
-        int[][] dist = new int[10][10];
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                pad[i][j] = keypad[i*3 + j];
+        int[][] key = new int[10][2];
+        int count = -1;
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
+                count++;
+                key[keypad[count]][0] = i;
+                key[keypad[count]][1] = j;
             }
         }
-        for(int i = 0; i < 10; i++){
-            Arrays.fill(dist[i], 2);
+
+        int first = Integer.parseInt(String.valueOf(password.charAt(0)));
+        int nowI = key[first][0];
+        int nowJ = key[first][1];
+
+        for (int i=1; i<password.length(); i++) {
+            int next = Integer.parseInt(String.valueOf(password.charAt(i)));
+            int nextI = key[next][0];
+            int nextJ = key[next][1];
+            int length = Math.max(Math.abs(nowI-nextI), Math.abs(nowJ-nextJ));
+            answer += length;
+            nowI = nextI;
+            nowJ = nextJ;
+//            System.out.print(i+"번째 길이는 = "+length+" / ");
         }
-        for(int i = 0; i < 10; i++) dist[i][i] = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                int from = pad[i][j];
-                for(int k = 0; k < 8; k++){
-                    if(i+dx[k] >= 0 && i+dx[k] < 3 && j+dy[k] >= 0 && j+dy[k] < 3){
-                        int to = pad[i+dx[k]][j+dy[k]];
-                        dist[from][to] = 1;
-                    }
-                }
-            }
-        }
-        for(int i = 1; i < password.length(); i++){
-            int from = (int)password.charAt(i-1)-48;
-            int to = (int)password.charAt(i)-48;
-            answer += dist[from][to];
-        }
+
         return answer;
     }
 
