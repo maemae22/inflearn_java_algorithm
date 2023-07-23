7c203097081aeffa3b6324ea0f1188d4932302e8
maemae22 <maemae2@naver.com>
2023-07-24 00:28:28 +0900

    P02_겹쳐진압축해제

---
+++ 섹션 3. 자료구조 활용/P02_겹쳐진압축해제.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class P02_겹쳐진압축해제 {
+
+    public String solution(String s) {
+        String answer = "";
+        Stack<String> st = new Stack<>();
+        for(Character x : s.toCharArray()){
+            if(x == ')'){
+                String tmp = "";
+                while(!st.empty()){
+                    String c = st.pop();
+                    if(c.equals("(")){
+                        String num = "";
+                        while(!st.empty() && Character.isDigit(st.peek().charAt(0))){
+                            num = st.pop() + num;
+                        }
+                        String res = "";
+                        int cnt = 0;
+                        if(num.equals("")) cnt = 1;
+                        else cnt = Integer.parseInt(num);
+                        for(int i = 0; i < cnt; i++) res += tmp;
+                        st.push(res);
+                        break;
+                    }
+                    tmp = c + tmp;
+                }
+            }
+            else st.push(String.valueOf(x));
+        }
+        for(String x : st) answer += x;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        P02_겹쳐진압축해제 T = new P02_겹쳐진압축해제();
+        System.out.println(T.solution("3(a2(b))ef"));
+        System.out.println(T.solution("2(ab)k3(bc)"));
+        System.out.println(T.solution("2(ab3((cd)))"));
+        System.out.println(T.solution("2(2(ab)3(2(ac)))"));
+        System.out.println(T.solution("3(ab2(sg))"));
+    }
+}
