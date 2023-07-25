65f94b2ec26c12539eed915e9c5c3f5e742cf15c
maemae22 <maemae2@naver.com>
2023-07-25 19:38:54 +0900

    P03_현관문출입순서 (풀이 추가)

--- 섹션 3. 자료구조 활용/P03_현관문출입순서.java
+++ 섹션 3. 자료구조 활용/P03_현관문출입순서.java
@@ -64,11 +64,58 @@ public class P03_현관문출입순서 {
         return answer;
     }
 
+    public int[] solution2(int[] arrival, int[] state) {
+        int[] answer = new int[arrival.length];
+        Queue<Integer> in = new LinkedList<>();
+        Queue<Integer> out = new LinkedList<>();
+
+        int now = arrival[0];
+        int recent = 1;
+
+        int index = 0;
+        while (index<=arrival.length) {
+            while (index<arrival.length && arrival[index]<=now) {
+                if (state[index]==1) {
+                    out.offer(index++);
+                } else {
+                    in.offer(index++);
+                }
+            }
+
+            if (recent==1 && !out.isEmpty()) {
+                recent = 1;
+                answer[out.poll()]=now++;
+            } else if (recent==1 && out.isEmpty() && !in.isEmpty()) {
+                recent = 0;
+                answer[in.poll()]=now++;
+            } else if (recent==0 && !in.isEmpty()) {
+                recent = 0;
+                answer[in.poll()]=now++;
+            } else if (recent==0 && in.isEmpty() && !out.isEmpty()) {
+                recent = 1;
+                answer[out.poll()]=now++;
+            } else {
+                recent = 1;
+                now++;
+            }
+
+            if (index==arrival.length && in.isEmpty() && out.isEmpty()) {
+                break;
+            }
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         P03_현관문출입순서 T = new P03_현관문출입순서();
         System.out.println(Arrays.toString(T.solution(new int[]{0, 1, 1, 1, 2, 3, 8, 8}, new int[]{1, 0, 0, 1, 0, 0, 1, 0})));
         System.out.println(Arrays.toString(T.solution(new int[]{3, 3, 4, 5, 5, 5}, new int[]{1, 0, 1, 0, 1, 0})));
         System.out.println(Arrays.toString(T.solution(new int[]{2, 2, 2, 3, 4, 8, 8, 9, 10, 10}, new int[]{1, 0, 0, 0, 1, 1, 0, 1, 1, 0})));
+        System.out.println();
+        System.out.println(Arrays.toString(T.solution2(new int[]{0, 1, 1, 1, 2, 3, 8, 8}, new int[]{1, 0, 0, 1, 0, 0, 1, 0})));
+        System.out.println(Arrays.toString(T.solution2(new int[]{3, 3, 4, 5, 5, 5}, new int[]{1, 0, 1, 0, 1, 0})));
+        System.out.println(Arrays.toString(T.solution2(new int[]{2, 2, 2, 3, 4, 8, 8, 9, 10, 10}, new int[]{1, 0, 0, 0, 1, 1, 0, 1, 1, 0})));
     }
 }
 
