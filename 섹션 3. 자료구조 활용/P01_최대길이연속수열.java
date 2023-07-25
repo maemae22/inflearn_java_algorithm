025f5ac46770592daa769366736e932ffd169128
maemae22 <maemae2@naver.com>
2023-07-25 15:19:22 +0900

    P01_최대길이연속수열 (풀이 추가)

--- 섹션 3. 자료구조 활용/P01_최대길이연속수열.java
+++ 섹션 3. 자료구조 활용/P01_최대길이연속수열.java
@@ -52,6 +52,27 @@ public class P01_최대길이연속수열 {
         return answer;
     }
 
+    public int solution3(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int answer = 1;
+        for (int num : set) {
+            if (!set.contains(num-1)) {
+                int count = 1;
+                while (set.contains(num+1)) {
+                    count++;
+                    num++;
+                }
+                answer = Math.max(answer, count);
+            }
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         P01_최대길이연속수열 T = new P01_최대길이연속수열();
         System.out.println(T.solution(new int[]{8, 1, 9, 3, 10, 2, 4, 0, 2, 3}));
@@ -65,5 +86,11 @@ public class P01_최대길이연속수열 {
         System.out.println(T.solution2(new int[]{3, 3, 3, 3, 3, 3, 3, 3}));
         System.out.println(T.solution2(new int[]{-3, -1, -2, 0, 3, 3, 5, 6, 2, 2, 1, 1}));
         System.out.println(T.solution2(new int[]{-5, -3, -1, -4, 3, 3, 5, 6, 2, 2, 1, 1, 7}));
+        System.out.println();
+        System.out.println(T.solution3(new int[]{8, 1, 9, 3, 10, 2, 4, 0, 2, 3}));
+        System.out.println(T.solution3(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0}));
+        System.out.println(T.solution3(new int[]{3, 3, 3, 3, 3, 3, 3, 3}));
+        System.out.println(T.solution3(new int[]{-3, -1, -2, 0, 3, 3, 5, 6, 2, 2, 1, 1}));
+        System.out.println(T.solution3(new int[]{-5, -3, -1, -4, 3, 3, 5, 6, 2, 2, 1, 1, 7}));
     }
 }
