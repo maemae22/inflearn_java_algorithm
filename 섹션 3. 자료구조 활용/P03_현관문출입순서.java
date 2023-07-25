03e8c68edff3fc7dfc4761d06dc7be77de516858
maemae22 <maemae2@naver.com>
2023-07-25 17:19:41 +0900

    P03_현관문출입순서 (풀이 수정)

--- 섹션 3. 자료구조 활용/P03_현관문출입순서.java
+++ 섹션 3. 자료구조 활용/P03_현관문출입순서.java
@@ -2,43 +2,65 @@ import java.util.*;
 
 public class P03_현관문출입순서 {
     public int[] solution(int[] arrival, int[] state) {
-        Queue<Integer> enter = new LinkedList<>();
-        Queue<Integer> exit = new LinkedList<>();
-        int n = arrival.length, prev = 1;
-        int[] answer = new int[n];
-        for(int t = 0, i = 0, cnt = 0; ; t++){
-            if(enter.isEmpty() && exit.isEmpty() && i < n) {
-                if(t < arrival[i]){
-                    t = arrival[i];
-                    prev = 1;
-                }
+        int N = arrival.length;
+
+        Queue<Worker> in = new LinkedList<>();
+        Queue<Worker> out = new LinkedList<>();
+        for (int i=0; i<N; i++) {
+            if (state[i]==0) {
+                in.add(new Worker(i, arrival[i], state[i]));
+            } else {
+                out.add(new Worker(i, arrival[i], state[i]));
             }
-            while(i < n && arrival[i] <= t) {
-                if (state[i] == 0) enter.offer(i);
-                else exit.offer(i);
-                i++;
-            }
-            if(prev == 1) {
-                if(!exit.isEmpty()) {
-                    answer[exit.poll()] = t;
-                    prev = 1;
-                }
-                else{
-                    answer[enter.poll()] = t;
-                    prev = 0;
-                }
-            }else if(prev == 0) {
-                if(!enter.isEmpty()) {
-                    answer[enter.poll()] = t;
-                    prev = 0;
-                }else{
-                    answer[exit.poll()] = t;
-                    prev = 1;
-                }
-            }
-            cnt++;
-            if(cnt == n) break;
         }
+
+        int now = arrival[0];
+        int recent = 1;
+        boolean find = false;
+
+        int[] answer = new int[N];
+        while (!in.isEmpty() || !out.isEmpty()) {
+            find = false;
+            if (recent==1) {
+                if (!out.isEmpty() && out.peek().time<=now) {
+                    Worker pick = out.poll();
+                    answer[pick.num] = now++;
+                    find = true;
+                    recent = 1;
+                }
+                if (!find) {
+                    if (!in.isEmpty() && in.peek().time<=now) {
+                        Worker pick = in.poll();
+                        answer[pick.num] = now++;
+                        find = true;
+                        recent = 0;
+                    }
+                }
+            }
+
+            if (recent==0) {
+                if (!in.isEmpty() && in.peek().time<=now) {
+                    Worker pick = in.poll();
+                    answer[pick.num] = now++;
+                    find = true;
+                    recent = 0;
+                }
+                if (!find) {
+                    if (!out.isEmpty() && out.peek().time<=now) {
+                        Worker pick = out.poll();
+                        answer[pick.num] = now++;
+                        find = true;
+                        recent = 1;
+                    }
+                }
+            }
+
+            if (!find) {
+                recent = 1;
+                now++;
+            }
+        }
+
         return answer;
     }
 
@@ -47,5 +69,16 @@ public class P03_현관문출입순서 {
         System.out.println(Arrays.toString(T.solution(new int[]{0, 1, 1, 1, 2, 3, 8, 8}, new int[]{1, 0, 0, 1, 0, 0, 1, 0})));
         System.out.println(Arrays.toString(T.solution(new int[]{3, 3, 4, 5, 5, 5}, new int[]{1, 0, 1, 0, 1, 0})));
         System.out.println(Arrays.toString(T.solution(new int[]{2, 2, 2, 3, 4, 8, 8, 9, 10, 10}, new int[]{1, 0, 0, 0, 1, 1, 0, 1, 1, 0})));
+    }
+}
+
+class Worker {
+    int num;
+    int time;
+    int state;
+    Worker (int num, int time, int state) {
+        this.num = num;
+        this.time = time;
+        this.state = state;
     }
 }
