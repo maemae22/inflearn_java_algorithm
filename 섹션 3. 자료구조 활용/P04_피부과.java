92aa35a66967e86fada22d2c28f429c31ae88eb0
maemae22 <maemae2@naver.com>
2023-07-27 13:27:28 +0900

    P04_피부과 (풀이 추가)

--- 섹션 3. 자료구조 활용/P04_피부과.java
+++ 섹션 3. 자료구조 활용/P04_피부과.java
@@ -35,6 +35,37 @@ public class P04_피부과 {
         return answer;
     }
 
+    public int solution2(int[] laser, String[] enter) {
+
+        Queue<Wait> all = new LinkedList<>();
+        Queue<Wait> wait = new LinkedList<>();
+        for (int i=0; i<enter.length; i++) {
+            String time = enter[i].split(" ")[0];
+            int a = stringTimeToInt(time);
+            int b = laser[Integer.parseInt(enter[i].split(" ")[1])];
+            all.offer(new Wait(a, b));
+        }
+
+        int answer = 0;
+        int now = 0;
+
+        while (!all.isEmpty()) {
+            if (!wait.isEmpty()) {
+                now += wait.poll().laser;
+            } else {
+                Wait tmp = all.poll();
+                now = tmp.time + tmp.laser;
+            }
+
+            while (!all.isEmpty() && all.peek().time < now) {
+                wait.offer(all.poll());
+            }
+            answer = Math.max(answer, wait.size());
+        }
+
+        return answer;
+    }
+
     public int stringTimeToInt(String time) {
         int hour = Integer.parseInt(time.substring(0, 2));
         int minute = Integer.parseInt(time.substring(3));
@@ -46,5 +77,18 @@ public class P04_피부과 {
         System.out.println(T.solution(new int[]{30, 20, 25, 15}, new String[]{"10:23 0", "10:40 3", "10:42 2", "10:52 3", "11:10 2"}));
         System.out.println(T.solution(new int[]{30, 20, 25, 15}, new String[]{"10:23 0", "10:40 3", "10:42 2", "10:52 3", "15:10 0", "15:20 3", "15:22 1", "15:23 0", "15:25 0"}));
         System.out.println(T.solution(new int[]{30, 20, 25, 15}, new String[]{"10:20 1", "10:40 1", "11:00 1", "11:20 1", "11:40 1"}));
+        System.out.println();
+        System.out.println(T.solution2(new int[]{30, 20, 25, 15}, new String[]{"10:23 0", "10:40 3", "10:42 2", "10:52 3", "11:10 2"}));
+        System.out.println(T.solution2(new int[]{30, 20, 25, 15}, new String[]{"10:23 0", "10:40 3", "10:42 2", "10:52 3", "15:10 0", "15:20 3", "15:22 1", "15:23 0", "15:25 0"}));
+        System.out.println(T.solution2(new int[]{30, 20, 25, 15}, new String[]{"10:20 1", "10:40 1", "11:00 1", "11:20 1", "11:40 1"}));
+    }
+}
+
+class Wait {
+    int time;
+    int laser;
+    Wait(int time, int laser) {
+        this.time = time;
+        this.laser = laser;
     }
 }
