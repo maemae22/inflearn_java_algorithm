bf3b95c64658bd4dca98743655690fc424d4bc60
maemae22 <maemae2@naver.com>
2023-07-29 22:45:41 +0900

    P02_줄다리기

---
+++ 섹션 6. 깊이우선탐색(DFS)/P02_줄다리기.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class P02_줄다리기 {
+
+    static int answer;
+    static HashMap<Integer, ArrayList<Integer>> hate;
+
+    public int solution(int[][] fight) {
+        answer = 0;
+        hate = new HashMap<>();
+        for (int i=0; i<=7; i++) {
+            hate.put(i, new ArrayList<>());
+        }
+
+        for (int i=0; i<fight.length; i++) {
+            int a = fight[i][0];
+            int b = fight[i][1];
+
+            ArrayList<Integer> aput = hate.get(a);
+            aput.add(b);
+            hate.put(a, aput);
+
+            ArrayList<Integer> bput = hate.get(b);
+            bput.add(a);
+            hate.put(b, bput);
+        }
+
+        DFS(0, new int[8], 0);
+        return answer;
+    }
+
+    public void DFS(int count, int[] check, int pri) {
+        if (count==7) {
+            answer++;
+        } else {
+            for (int i=1; i<8; i++) {
+                if (check[i]==0 && !hate.get(pri).contains(i)) {
+                    check[i]=1;
+                    DFS(count+1, check, i);
+                    check[i]=0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        P02_줄다리기 T = new P02_줄다리기();
+        System.out.println(T.solution(new int[][]{{1, 3}, {5, 7}, {4, 2}}));
+        System.out.println(T.solution(new int[][]{{3, 2}, {3, 5}, {5, 2}, {7, 3}}));
+        System.out.println(T.solution(new int[][]{{1, 2}, {1, 5}, {1, 7}, {1, 3}}));
+        System.out.println(T.solution(new int[][]{{1, 7}}));
+        System.out.println(T.solution(new int[][]{{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}}));
+    }
+}
