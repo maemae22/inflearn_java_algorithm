5091579a3ebd8e530d463d51a6b77e4deb2847f9
maemae22 <maemae2@naver.com>
2023-08-05 00:16:43 +0900

    P06_가장많이사용된회의실 (풀이 추가)

--- 섹션 3. 자료구조 활용/P06_가장많이사용된회의실.java
+++ 섹션 3. 자료구조 활용/P06_가장많이사용된회의실.java
@@ -6,7 +6,7 @@ public class P06_가장많이사용된회의실 {
         int[] times = new int[n];
         PriorityQueue<Meet> meeting = new PriorityQueue<>();
         for (int[] meets : meetings) {
-            meeting.add(new Meet(meets[0], meets[1], meets[1]-meets[0]));
+            meeting.add(new Meet(meets[0], meets[1]));
         }
 
         for (Meet tmp : meeting) {
@@ -22,11 +22,7 @@ public class P06_가장많이사용된회의실 {
 
                 if (times[i]<=tmp.start) {
                     check[i]++;
-                    if (times[i]==tmp.start) {
-                        times[i]+=tmp.time;
-                    } else {
-                        times[i]=tmp.end;
-                    }
+                    times[i]=tmp.end;
                     find = true;
                     break;
                 }
@@ -34,7 +30,47 @@ public class P06_가장많이사용된회의실 {
 
             if (!find) {
                 check[room]++;
-                times[room]+=tmp.time;
+                times[room]+=(tmp.end-tmp.start);
+            }
+        }
+
+        int max = 0;
+        int answer = -1;
+        for (int i=0; i<n; i++) {
+            if (check[i]>max) {
+                max = check[i];
+                answer = i;
+            }
+        }
+
+        return answer;
+    }
+
+    public int solution2(int n, int[][] meetings) {
+        int[] check = new int[n];
+        PriorityQueue<Meet> meeting = new PriorityQueue<>();
+        for (int[] meets : meetings) {
+            meeting.add(new Meet(meets[0], meets[1]));
+        }
+        TreeSet<Integer> empty = new TreeSet<>();
+        for (int i=0; i<n; i++) {
+            empty.add(i);
+        }
+        PriorityQueue<Meet> now = new PriorityQueue<>();
+
+        for (Meet tmp : meeting) {
+            while (!now.isEmpty() && now.peek().start<=tmp.start) {
+                Meet out = now.poll();
+                empty.add(out.end);
+            }
+            if (!empty.isEmpty()) {
+                int room = empty.pollFirst();
+                check[room]++;
+                now.add(new Meet(tmp.end, room));
+            } else {
+                Meet out = now.poll();
+                check[out.end]++;
+                now.add(new Meet(out.start+tmp.end-tmp.start, out.end));
             }
         }
 
@@ -56,22 +92,25 @@ public class P06_가장많이사용된회의실 {
         System.out.println(T.solution(3, new int[][]{{3, 9}, {1, 10}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}}));
         System.out.println(T.solution(3, new int[][]{{1, 30}, {2, 15}, {3, 10}, {4, 12}, {6, 10}}));
         System.out.println(T.solution(4, new int[][]{{3, 20}, {1, 25}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}}));
+        System.out.println();
+        System.out.println(T.solution2(2, new int[][]{{0, 5}, {2, 7}, {4, 5}, {7, 10}, {9, 12}}));
+        System.out.println(T.solution2(3, new int[][]{{3, 9}, {1, 10}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}}));
+        System.out.println(T.solution2(3, new int[][]{{1, 30}, {2, 15}, {3, 10}, {4, 12}, {6, 10}}));
+        System.out.println(T.solution2(4, new int[][]{{3, 20}, {1, 25}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}}));
     }
 }
 
 class Meet implements Comparable<Meet> {
     int start;
     int end;
-    int time;
 
-    Meet(int start, int end, int time) {
+    Meet(int start, int end) {
         this.start = start;
         this.end = end;
-        this.time = time;
     }
 
     @Override
     public int compareTo(Meet o) {
-        return this.start - o.start;
+        return this.start!=o.start ? this.start-o.start : this.end-o.end;
     }
 }
