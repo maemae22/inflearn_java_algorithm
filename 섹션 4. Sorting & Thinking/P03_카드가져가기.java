18f2b4279d324ed3858051b166006ae9305932ca
maemae22 <maemae2@naver.com>
2023-08-26 15:45:38 +0900

    P03_카드가져가기 (풀이 수정)

--- 섹션 4. Sorting & Thinking/P03_카드가져가기.java
+++ 섹션 4. Sorting & Thinking/P03_카드가져가기.java
@@ -2,19 +2,30 @@ import java.util.*;
 
 public class P03_카드가져가기 {
     public int solution(int[] nums, int k) {
+        int[] arr = nums.clone();
+        Arrays.sort(arr);
+
+        int round = nums.length/2;
+        List<Minus> list = new ArrayList<>();
+        for (int i=0; i<round; i++) {
+            list.add(new Minus(i, arr[i*2+1] - arr[i*2]));
+        }
+        Collections.sort(list);
+
+        List<Integer> pick = new ArrayList<>();
+        for (int i=0; i<k; i++) {
+            pick.add(list.get(i).index);
+        }
+
         int answer = 0;
-        int n = nums.length;
-        Integer[] tmp = Arrays.stream(nums).boxed().toArray(Integer[]::new);
-        Arrays.sort(tmp, (a, b) -> b - a);
-        Integer[] diff = new Integer[n/2];
-        for(int i = 0; i < n/2; i++){
-            answer += tmp[i*2+1];
-            diff[i] = tmp[i*2]-tmp[i*2+1];
+        for (int i=0; i<round; i++) {
+            if (pick.contains(i)) {
+                answer += arr[2*i+1];
+            } else {
+                answer += arr[2*i];
+            }
         }
-        Arrays.sort(diff,(a, b) -> b - a);
-        for(int i = 0; i < k; i++){
-            answer += diff[i];
-        }
+
         return answer;
     }
 
@@ -25,5 +36,18 @@ public class P03_카드가져가기 {
         System.out.println(T.solution(new int[]{3, 7, 12, 3, 3, 5, 7, 8, 9, 11, 23, 4, 6, 7}, 3));
         System.out.println(T.solution(new int[]{12, 34, 56, 23, 22, 34, 55, 45, 24, 23, 45, 55, 55, 23, 11, 12, 23, 12}, 3));
         System.out.println(T.solution(new int[]{14, 15, 20, 11, 10, 20, 20, 12, 9, 22, 27, 25, 30, 19}, 3));
+    }
+}
+
+class Minus implements Comparable<Minus> {
+    int index;
+    int value;
+    Minus(int index, int value) {
+        this.index = index;
+        this.value = value;
+    }
+    @Override
+    public int compareTo(Minus o) {
+        return o.value - this.value;
     }
 }
