832a9b7212599d62760c91979d0a50f2a3f979ad
maemae22 <maemae2@naver.com>
2023-12-05 22:11:22 +0900

    P07_회문문자열

---
+++ 1. String(문자열)/복습_새로운시작/P07_회문문자열.java
@@ -0,0 +1,18 @@
+package 복습_새로운시작;
+
+import java.util.*;
+import java.io.*;
+
+public class P07_회문문자열 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String letter = br.readLine().toUpperCase();
+        String reverse = new StringBuilder(letter).reverse().toString();
+        if (letter.equals(reverse)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
