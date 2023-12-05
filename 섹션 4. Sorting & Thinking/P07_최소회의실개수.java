3649f12ab5112c8e44e5cc135190798b7dc0a826
maemae22 <maemae2@naver.com>
2023-12-05 15:54:21 +0900

    P07_최소회의실개수 (풀이 추가)

--- 섹션 4. Sorting & Thinking/P07_최소회의실개수.java
+++ 섹션 4. Sorting & Thinking/P07_최소회의실개수.java
@@ -2,18 +2,53 @@ import java.util.*;
 
 public class P07_최소회의실개수 {
     public int solution(int[][] meetings) {
-        ArrayList<int[]> list = new ArrayList<>();
-        for(int[] x : meetings){
-            list.add(new int[]{x[0], 1});
-            list.add(new int[]{x[1], 2});
+        int[] in = new int[meetings.length];
+        int[] out = new int[meetings.length];
+        for (int i=0; i<meetings.length; i++) {
+            in[i] = meetings[i][0];
+            out[i] = meetings[i][1];
         }
-        list.sort((a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
-        int answer = 0, cnt = 0;
-        for(int[] x : list){
-            if(x[1] == 1) cnt++;
-            else cnt--;
-            answer = Math.max(answer, cnt);
+
+        Arrays.sort(in);
+        Arrays.sort(out);
+
+        int answer = -1;
+        int outIndex = 0;
+        int now = 0;
+        for (int i=0; i<in.length; i++) {
+            while (out[outIndex]<=in[i]) {
+                outIndex++;
+                now--;
+            }
+            now++;
+            answer = Math.max(answer, now);
         }
+
+        return answer;
+    }
+
+    public int solution2(int[][] meetings) {
+        List<Time> list = new ArrayList<>();
+        for (int i=0; i<meetings.length; i++) {
+            Time time1 = new Time(meetings[i][0], 1);
+            Time time2 = new Time(meetings[i][1], 2);
+            list.add(time1);
+            list.add(time2);
+        }
+
+        Collections.sort(list);
+
+        int answer = -1;
+        int now = 0;
+        for (Time tmp : list) {
+            if (tmp.inout==1) {
+                now++;
+            } else {
+                now--;
+            }
+            answer = Math.max(answer, now);
+        }
+
         return answer;
     }
 
@@ -23,5 +58,29 @@ public class P07_최소회의실개수 {
         System.out.println(T.solution(new int[][]{{1, 30}, {2, 15}, {3, 10}, {4, 12}, {6, 10}}));
         System.out.println(T.solution(new int[][]{{3, 9}, {1, 10}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}}));
         System.out.println(T.solution(new int[][]{{0, 5}, {2, 7}, {4, 5}, {7, 10}, {9, 12}}));
+        System.out.println();
+        System.out.println("2번째 풀이");
+        System.out.println(T.solution2(new int[][]{{0, 10}, {20, 25}, {5, 15}, {2, 5}}));
+        System.out.println(T.solution2(new int[][]{{1, 30}, {2, 15}, {3, 10}, {4, 12}, {6, 10}}));
+        System.out.println(T.solution2(new int[][]{{3, 9}, {1, 10}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}}));
+        System.out.println(T.solution2(new int[][]{{0, 5}, {2, 7}, {4, 5}, {7, 10}, {9, 12}}));
+    }
+}
+
+class Time implements Comparable<Time> {
+    int time;
+    int inout;
+    Time(int time, int inout) {
+        this.time = time;
+        this.inout = inout;
+    }
+
+    @Override
+    public int compareTo(Time o) {
+        // 정렬 방법
+        // 1. 시간(time)을 기준으로 오름차순 정렬한다.
+        // 2. 시간이 같을 경우, inout을 기준으로 내림차순 정렬한다.
+        //    (in=1, out=2 인데 out이 먼저 카운트되어야 하기 때문에 out(2)->in(1) 내림차순 정렬 필요)
+        return this.time==o.time ? o.inout-this.inout : this.time-o.time;
     }
 }
