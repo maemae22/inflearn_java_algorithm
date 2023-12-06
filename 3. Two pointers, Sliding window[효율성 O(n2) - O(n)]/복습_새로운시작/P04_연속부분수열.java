9ca675d2669a053ce06838eb9acd9be4d7a1a3a1
maemae22 <maemae2@naver.com>
2023-12-06 21:06:20 +0900

    P04_연속부분수열

---
+++ 3. Two pointers, Sliding window[효율성 O(n2) - O(n)]/복습_새로운시작/P04_연속부분수열.java
@@ -0,0 +1,40 @@
+package 복습_새로운시작;
+
+import java.util.*;
+import java.io.*;
+
+public class P04_연속부분수열 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 0;
+        int now = 0;
+        int lt = 0;
+        for (int rt=0; rt<N; rt++) {
+            now += arr[rt];
+            if (now==M) {
+                answer++;
+            } else if (now<M) {
+
+            } else { // now>M
+                while (now>M) {
+                    now -= arr[lt++];
+                }
+                if (now==M) {
+                    answer++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
