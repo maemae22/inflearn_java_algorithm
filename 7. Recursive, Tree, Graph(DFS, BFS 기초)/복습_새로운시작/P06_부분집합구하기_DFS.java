cc5aef96afef69f775f51fdf0986c0cd142a3e4a
maemae22 <maemae2@naver.com>
2023-12-09 18:21:30 +0900

    P06_부분집합구하기_DFS

---
+++ 7. Recursive, Tree, Graph(DFS, BFS 기초)/복습_새로운시작/P06_부분집합구하기_DFS.java
@@ -0,0 +1,38 @@
+package 복습_새로운시작;
+
+import java.util.*;
+import java.io.*;
+
+public class P06_부분집합구하기_DFS {
+
+    static int[] check;
+    static int N;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        check = new int[N+1];
+        DFS(1);
+    }
+
+    public static void DFS(int L) {
+        if (L==N+1) {
+            StringBuilder sb = new StringBuilder();
+            for (int i=1; i<=N; i++) {
+                if (check[i]==1) {
+                    sb.append(i).append(" ");
+                }
+            }
+
+            if (sb.length()>0) {
+                System.out.println(sb.toString());
+            }
+        } else {
+            check[L]=1;
+            DFS(L+1);
+
+            check[L]=0;
+            DFS(L+1);
+        }
+    }
+}
