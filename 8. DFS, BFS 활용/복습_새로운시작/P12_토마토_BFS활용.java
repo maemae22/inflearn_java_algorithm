3d8b2396ffbb72f1d23e9957dc115d9f3a49c073
maemae22 <maemae2@naver.com>
2023-12-13 20:13:49 +0900

    P12_토마토_BFS활용

---
+++ 8. DFS, BFS 활용/복습_새로운시작/P12_토마토_BFS활용.java
@@ -0,0 +1,73 @@
+package 복습_새로운시작;
+
+import java.util.*;
+import java.io.*;
+
+public class P12_토마토_BFS활용 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j=0; j<M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        System.out.println(BFS(arr));
+    }
+
+    public static int BFS(int[][] arr) {
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+
+        Queue<Tomato> q = new LinkedList<>();
+        for (int i=0; i<arr.length; i++) {
+            for (int j=0; j<arr[0].length; j++) {
+                if (arr[i][j]==1) {
+                    q.offer(new Tomato(i, j));
+                }
+            }
+        }
+
+        int day = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i=0; i<size; i++) {
+                Tomato tmp = q.poll();
+                for (int k=0; k<4; k++) {
+                    int nx = tmp.x+dx[k];
+                    int ny = tmp.y+dy[k];
+                    if (0<=nx && nx<arr.length && 0<=ny && ny<arr[0].length && arr[nx][ny]==0) {
+                        arr[nx][ny]=1;
+                        q.offer(new Tomato(nx, ny));
+                    }
+                }
+            }
+            day++;
+        }
+
+        for (int i=0; i<arr.length; i++) {
+            for (int j=0; j<arr[0].length; j++) {
+                if (arr[i][j]==0) {
+                    return -1;
+                }
+            }
+        }
+
+        return day-1;
+    }
+}
+
+class Tomato {
+    int x;
+    int y;
+    Tomato(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
