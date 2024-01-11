76652b5cbe5e1b6e2406933c000c8658d2417c09
maemae22 <maemae2@naver.com>
2024-01-11 16:21:57 +0900

    P03_스프링쿨러

--- 섹션 5. greedy(탐욕법)/P03_스프링쿨러.java
+++ 섹션 5. greedy(탐욕법)/P03_스프링쿨러.java
@@ -2,24 +2,33 @@ import java.util.*;
 
 public class P03_스프링쿨러 {
     public int solution(int n, int[] nums) {
-        int answer = 0;
-        int[][] line = new int[nums.length + 1][2];
-        for(int i = 0; i <= n; i++){
-            line[i][0] = Math.max(0, i - nums[i]);
-            line[i][1] = Math.min(n, i + nums[i]);
-        }
-        Arrays.sort(line, (a, b) -> a[0] - b[0]);
-        int start = 0, end = 0, i = 0;
-        while(i < line.length){
-            while(i < line.length && line[i][0] <= start){
-                end = Math.max(end, line[i][1]);
-                i++;
+        List<Spring> list = new ArrayList<>();
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i]!=0) {
+                list.add(new Spring(i-nums[i], i+nums[i]));
             }
-            answer++;
-            if(end == n) return answer;
-            if(start == end) return -1;
-            start = end;
         }
+
+        Collections.sort(list);
+        int answer = 0;
+        int index = 0;
+        int end = 0;
+        while (index<list.size() && end<n) {
+            if (end<list.get(index).start) {
+                return -1;
+            } else {
+                int tmpEnd = end;
+                while (index<list.size() && list.get(index).start<=end) {
+                    tmpEnd = Math.max(tmpEnd, list.get(index).end);
+                    index++;
+                }
+                if (end!=tmpEnd) {
+                    answer++;
+                }
+                end = tmpEnd;
+            }
+        }
+
         return answer;
     }
 
@@ -29,5 +38,19 @@ public class P03_스프링쿨러 {
         System.out.println(T.solution(4, new int[]{1, 2, 2, 0, 0}));
         System.out.println(T.solution(5, new int[]{2, 0, 0, 0, 0, 2}));
         System.out.println(T.solution(11, new int[]{1, 2, 3, 1, 2, 1, 1, 2, 1, 1, 1, 1}));
+    }
+}
+
+class Spring implements Comparable<Spring> {
+    int start;
+    int end;
+    Spring(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public int compareTo(Spring o) {
+        return this.start!=o.start ? this.start-o.start : o.end-this.end;
     }
 }
