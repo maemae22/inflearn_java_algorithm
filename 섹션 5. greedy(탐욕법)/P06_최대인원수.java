d25eb28851e327f6e6f322f1f59f74d4f69007f3
maemae22 <maemae2@naver.com>
2024-01-11 20:41:36 +0900

    P06_최대인원수

--- 섹션 5. greedy(탐욕법)/P06_최대인원수.java
+++ 섹션 5. greedy(탐욕법)/P06_최대인원수.java
@@ -2,33 +2,36 @@ import java.util.*;
 
 public class P06_최대인원수 {
     public int solution(int n, int[][] trans, int[][] bookings) {
-        int answer=0;
-        int[] sum = new int[n+1];
-        for(int[] x : trans){
-            sum[x[0]] += x[2];
-            sum[x[1]] -= x[2];
+        int[] road = new int[n];
+        for (int i=0; i<trans.length; i++) {
+            for (int j=trans[i][0]; j<trans[i][1]; j++) {
+                road[j] += trans[i][2];
+            }
         }
-        for(int i = 1; i <= n; i++){
-            sum[i] += sum[i-1];
+
+        List<Train> list = new ArrayList<>();
+        for (int[] tmp : bookings) {
+            list.add(new Train(tmp[0], tmp[1]));
         }
-        int bN = bookings.length;
-        Arrays.sort(bookings, (a, b) -> a[0] - b[0]);
-        LinkedList<Integer> nums = new LinkedList<>();
-        int ix = 0;
-        for(int i = 1; i <= n; i++){
-            while(!nums.isEmpty() && nums.peek() == i){
+        Collections.sort(list);
+
+        int answer = 0;
+        for (Train tmp : list) {
+            boolean can = true;
+            for (int i=tmp.start; i<tmp.end; i++) {
+                if (road[i]<1) {
+                    can = false;
+                    break;
+                }
+            }
+            if (can) {
+                for (int i=tmp.start; i<tmp.end; i++) {
+                    road[i]--;
+                }
                 answer++;
-                nums.pollFirst();
-            }
-            while(ix < bN && bookings[ix][0] == i){
-                nums.add(bookings[ix][1]);
-                ix++;
-            }
-            Collections.sort(nums);
-            while(nums.size() > sum[i]){
-                nums.pollLast();
             }
         }
+
         return answer;
     }
 
@@ -39,5 +42,18 @@ public class P06_최대인원수 {
         System.out.println(T.solution(8, new int[][]{{1, 8, 3}, {3, 8, 1}}, new int[][]{{1, 3}, {5, 8}, {2, 7}, {3, 8}, {2, 7}, {2, 8}, {3, 8}, {6, 8}, {7, 8}, {5, 8}, {2, 5}, {2, 7}, {3, 7}, {3, 8}}));
         System.out.println(T.solution(9, new int[][]{{1, 8, 3}, {3, 9, 2}, {1, 5, 3}}, new int[][]{{1, 9}, {5, 8}, {2, 9}, {3, 8}, {2, 9}, {1, 9}, {8, 9}, {3, 9}, {1, 8}, {6, 8}, {7, 8}, {5, 8}, {3, 5}, {3, 7}, {4, 7}, {5, 8}}));
         System.out.println(T.solution(9, new int[][]{{2, 7, 2}, {3, 9, 2}, {1, 5, 3}}, new int[][]{{1, 9}, {4, 8}, {2, 9}, {5, 9}, {3, 8}, {2, 9}, {1, 9}, {8, 9}, {3, 9}, {1, 8}, {6, 8}, {3, 6}, {7, 8}, {5, 8}, {3, 5}, {2, 7}, {1, 7}, {2, 8}}));
+    }
+}
+
+class Train implements Comparable<Train> {
+    int start;
+    int end;
+    Train(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+    @Override
+    public int compareTo(Train o) {
+        return this.end-this.start-(o.end-o.start);
     }
 }
