30c5c434a274133fd30d9d9e72f4d4df4b06794d
maemae22 <maemae2@naver.com>
2024-01-15 17:18:29 +0900

    P03_B2579_계단오르기 / https://www.acmicpc.net/problem/2579

---
+++ 섹션 9. 다이나믹 프로그래밍(DP)/P03_B2579_계단오르기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+// https://www.acmicpc.net/problem/2579
+public class P03_B2579_계단오르기 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N+1][3];
+        int[] arr = new int[N+1];
+        for (int i=1; i<=N; i++) {
+            arr[i]=Integer.parseInt(br.readLine());
+        }
+
+        dp[1][1]=arr[1];
+        dp[1][0]=arr[1];
+        for (int i=2; i<=N; i++) {
+            dp[i][1] = dp[i-2][0]+arr[i];
+            dp[i][2] = dp[i-1][1]+arr[i];
+            dp[i][0] = Math.max(dp[i][1], dp[i][2]);
+        }
+
+        System.out.println(dp[N][0]);
+    }
+}
